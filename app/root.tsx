83f50b61cdee346057f989028a75bcd882a28496
Justin Fowler <jusfowle@gmail.com>
2023-01-24 22:21:34 -0500

    Set up routes and add Tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,23 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, LiveReload, Outlet } from '@remix-run/react';
+import type { LinksFunction } from '@remix-run/node';
+import styles from './tailwind.css';
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
+const App = () => {
+    return (
+        <html lang='en'>
+            <head>
+                <meta charSet='utf-8' />
+                <title>Remix: So great, it's funny!</title>
+                <Links />
+            </head>
+            <body>
+                <Outlet />
+                <LiveReload />
+            </body>
+        </html>
+    );
+};
+
+export default App;
---
+++ app/routes/result.tsx
@@ -0,0 +1,9 @@
+const Result = () => (
+    <div>
+        <h1>Result</h1>
+        <main>
+        </main>
+    </div>
+);
+
+export default Result;
